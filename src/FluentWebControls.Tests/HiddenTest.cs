2983df4ba5c85cd92a2216a47acb64a0175818b2
Clinton Sheppard <csheppard@mvbalaw.com>
2010-04-09 16:09:15 -0500

    rewrite HiddenTest using BDD framework

--- src/FluentWebControls.Tests/HiddenTest.cs
+++ src/FluentWebControls.Tests/HiddenTest.cs
@@ -1,4 +1,11 @@
-﻿using FluentAssert;
+﻿using System;
+using System.Collections.Generic;
+
+using FluentAssert;
+
+using FluentWebControls.Tests.Extensions;
+
+using MvbaCore;
 
 using NUnit.Framework;
 
@@ -7,65 +14,117 @@ namespace FluentWebControls.Tests
 	public class HiddenTest
 	{
 		[TestFixture]
-		public class When_asked_to_create_a_hidden_field_for_a_property_of_type_int
+		public class When_asked_to_create_a_hidden_field
 		{
+			private string _expectedId;
+			private string _expectedName;
+			private string _expectedValue;
+			private Dictionary<string, string> _parsedResult;
+			private string _result;
+
 			[Test]
-			public void Should_return_html_code_representing_a_hidden_field_with_its_value_embedded_in_it()
+			public void Given_a_source_property_of_type_int()
 			{
-				const int value = 10;
-				var foo = new Foo
-					{
-						Value = value
-					};
-				var hiddenData = Hidden.For(foo, x => x.Value.ToString(), x => x.Value);
-				hiddenData.ToString().ShouldBeEqualTo("<input type='hidden' id='value' name='value' value='" + value + "'/>");
+				Test.Given<HiddenData>()
+					.CreatedBy(Create_HiddenData_from_integer_context, new IntegerContext
+						{
+							Value = 10
+						})
+					.When(ToString_is_called)
+					.Should(Get_the_property_name_as_the_tag_id_attribute)
+					.Should(Get_the_property_name_as_the_tag_name_attribute)
+					.Should(Get_the_property_value_as_the_tag_value_attribute)
+					.Verify();
 			}
 
-			public class Foo
+			[Test]
+			public void Given_a_source_property_of_type_nullable_int()
+			{
+				Test.Given<HiddenData>()
+					.CreatedBy(Create_HiddenData_from_nullable_integer_context, new NullableIntegerContext
+						{
+							Value = 10
+						})
+					.When(ToString_is_called)
+					.Should(Get_the_property_name_as_the_tag_id_attribute)
+					.Should(Get_the_property_name_as_the_tag_name_attribute)
+					.Should(Get_the_property_value_as_the_tag_value_attribute)
+					.Verify();
+			}
+
+			[Test]
+			public void Given_a_source_property_of_type_string()
+			{
+				Test.Given<HiddenData>()
+					.CreatedBy(Create_HiddenData_from_string_context, new StringContext
+						{
+							Value = "Bar"
+						})
+					.When(ToString_is_called)
+					.Should(Get_the_property_name_as_the_tag_id_attribute)
+					.Should(Get_the_property_name_as_the_tag_name_attribute)
+					.Should(Get_the_property_value_as_the_tag_value_attribute)
+					.Verify();
+			}
+
+			private HiddenData Create_HiddenData_from_integer_context(IntegerContext context)
+			{
+				_expectedId = Reflection.GetPropertyName(() => context.Value).ToCamelCase();
+				_expectedName = _expectedId;
+				_expectedValue = context.Value.ToString();
+				return Hidden.For(context, x => x.Value.ToString(), x => x.Value);
+			}
+
+			private HiddenData Create_HiddenData_from_nullable_integer_context(NullableIntegerContext context)
+			{
+				_expectedId = Reflection.GetPropertyName(() => context.Value).ToCamelCase();
+				_expectedName = _expectedId;
+				_expectedValue = context.Value.ToString();
+				return Hidden.For(context, x => x.Value.ToString(), x => x.Value);
+			}
+
+			private HiddenData Create_HiddenData_from_string_context(StringContext context)
+			{
+				_expectedId = Reflection.GetPropertyName(() => context.Value).ToCamelCase();
+				_expectedName = _expectedId;
+				_expectedValue = context.Value;
+				return Hidden.For(context, x => x.Value.ToString(), x => x.Value);
+			}
+
+			private void Get_the_property_name_as_the_tag_id_attribute()
+			{
+				_parsedResult["id"].ShouldBeEqualTo(_expectedId);
+			}
+
+			private void Get_the_property_name_as_the_tag_name_attribute()
+			{
+				_parsedResult["name"].ShouldBeEqualTo(_expectedName);
+			}
+
+			private void Get_the_property_value_as_the_tag_value_attribute()
+			{
+				_parsedResult["value"].ShouldBeEqualTo(_expectedValue);
+			}
+
+			public class IntegerContext
 			{
 				public int Value { get; set; }
 			}
-		}
 
-		[TestFixture]
-		public class When_asked_to_create_a_hidden_field_for_a_property_of_type_nullable_int
-		{
-			[Test]
-			public void Should_return_html_code_representing_a_hidden_field_with_its_value_embedded_in_it()
-			{
-				int? value = 10;
-				var foo = new Foo
-					{
-						Value = value
-					};
-				var hiddenData = Hidden.For(foo, x => x.Value == null ? "" : x.Value.ToString(), x => x.Value);
-				hiddenData.ToString().ShouldBeEqualTo("<input type='hidden' id='value' name='value' value='" + value + "'/>");
-			}
-
-			public class Foo
+			public class NullableIntegerContext
 			{
 				public int? Value { get; set; }
 			}
-		}
 
-		[TestFixture]
-		public class When_asked_to_create_a_hidden_field_for_a_property_of_type_string
-		{
-			[Test]
-			public void Should_return_html_code_representing_a_hidden_field_with_its_value_embedded_in_it()
-			{
-				const string value = "Bar";
-				var foo = new Foo
-					{
-						Value = value
-					};
-				var hiddenData = Hidden.For(foo, x => x.Value);
-				hiddenData.ToString().ShouldBeEqualTo("<input type='hidden' id='value' name='value' value='" + value + "'/>");
-			}
-
-			public class Foo
+			public class StringContext
 			{
 				public string Value { get; set; }
+			}
+
+			private void ToString_is_called(HiddenData hiddenData)
+			{
+				_result = hiddenData.ToString();
+				_parsedResult = _result.ParseHtmlTag();
 			}
 		}
 	}
