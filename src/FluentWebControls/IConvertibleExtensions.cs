3ae159ec2a0f48d650e9651e123f8bddb1fcf777
Shashank Shetty <sshetty@mvbalaw.com>
2011-11-08 15:31:54 -0600

    added the ability to handle dollar symbol in populate mathod

--- src/FluentWebControls/IConvertibleExtensions.cs
+++ src/FluentWebControls/IConvertibleExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace FluentWebControls
 {
@@ -8,20 +10,26 @@ namespace FluentWebControls
 		{
 			// based on: http://stackoverflow.com/questions/793714/how-can-i-fix-this-up-to-do-generic-conversion-to-nullablet
 
-
 			var underlyingType = Nullable.GetUnderlyingType(targetType);
 
 			if (underlyingType != null)
 			{
 				if (obj == null)
+				{
 					return null;
+				}
 
-				return Convert.ChangeType(obj, underlyingType);
+				targetType = underlyingType;
 			}
-			else
+
+			if (obj is string && new List<Type>{typeof(decimal), typeof(double), typeof(int)}.Contains(targetType))
 			{
-				return Convert.ChangeType(obj, targetType);
+				double d;
+				Double.TryParse ((string)obj, NumberStyles.AllowCurrencySymbol | NumberStyles.AllowThousands | NumberStyles.AllowDecimalPoint, null, out d);
+				obj = d;
 			}
+
+			return Convert.ChangeType(obj, targetType);
 		}
 	}
 }
